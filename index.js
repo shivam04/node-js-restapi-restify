ba68316b86202980e8740572ecce8a0679ba00bc
shivam.si <shivam.si@MMMDVSHIVAMSI.local>
2019-08-08 19:50:37 +0530

    user auth api

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
 const config = require('./config');
+const rjwt = require('restify-jwt-community');
 
-//create server
 const server = restify.createServer();
 
-//middleware
-server.use(restify.plugins.bodyParser);
+// Middleware
+server.use(restify.plugins.bodyParser());
 
-//listen connection
+// Protect Routes
+//server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth'] }));
+
 server.listen(config.PORT, () => {
-    mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true }); //to avoid warning  useNewUrlParser:true
+    mongoose.set('useFindAndModify', false);
+    mongoose.connect(
+        config.MONGODB_URI,
+        { useNewUrlParser: true }
+    );
 });
 
-//initialize db variable
-
 const db = mongoose.connection;
-db.on('error', (err) => console.log(err));
+
+db.on('error', err => console.log(err));
 
 db.once('open', () => {
-    require('./routes/customers')(server); //use server to create routes
+    require('./routes/customers')(server);
+    require('./routes/users')(server);
     console.log(`Server started on port ${config.PORT}`);
 });
